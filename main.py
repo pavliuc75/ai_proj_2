f7e71591d0e52ce10c25674bd7659f886efb98e2
Ursu Adrian <uursu.adrian@gmail.com>
2024-04-22 17:20:06 +0200

    initial commit for the agent

--- main.py
+++ main.py
@@ -65,4 +65,62 @@ c = symbols('c')
 expression1 = (a >> b) & a
 expression2 = c
 
-print(revise(expression1, expression2))
+# print(revise(expression1, expression2))
+
+# ------------ ADRIAN ---------------#
+# Set up an initial list for storing beliefs
+knowledge_base = []
+
+# Add a new belief to the knowledge base
+def include_belief(knowledge_base, new_belief):
+    updated_base = revise(And(*knowledge_base), new_belief) if knowledge_base else new_belief
+    knowledge_base.append(to_cnf(updated_base))
+
+# Show all beliefs in the knowledge base
+def show_beliefs(knowledge_base):
+    print("Knowledge Base Contains:")
+    for belief in knowledge_base:
+        print(belief)
+
+
+# Remove all beliefs from the knowledge base
+def reset_beleifs(knowledge_base):
+    knowledge_base.clear()
+
+# Function to capture the user's command
+def user_command():
+    print("You can:")
+    print("1) add - Add a belief")
+    print("2) show - Show beliefs")
+    print("3) reset - Reset the belief base")
+    print("4) exit - Exit the program")
+    return input("Please choose an action: ").strip().lower()
+
+# Interpret and convert user input to a symbolic expression
+def interpret_belief(input_belief):
+    # This needs to be expanded based on how complex the input can be
+    return sympify(input_belief)
+
+# Loop to interact with the belief revision agent
+def interact_with_agent():
+    stop_agent = False
+    while not stop_agent:
+        user_choice = user_command()
+        if user_choice == "add":
+            belief_input = input("Enter the new belief: ")
+            try:
+                belief_expr = interpret_belief(belief_input)
+                include_belief(knowledge_base, belief_expr)
+            except SympifyError:
+                print("Could not interpret the belief.")
+        elif user_choice == "show":
+            show_beliefs(knowledge_base)
+        elif user_choice == "reset":
+            reset_beleifs(knowledge_base)
+        elif user_choice == "exit":
+            stop_agent = True
+        else:
+            print("Action not recognized")
+
+# Start interacting with the agent
+interact_with_agent()
