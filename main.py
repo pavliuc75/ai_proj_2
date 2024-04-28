1b742b69000025824fadbb96fa409353627d23cf
Tijana Basic <tba@eupry.com>
2024-04-28 17:56:33 +0200

    implement contraction and priority ordering

--- main.py
+++ main.py
@@ -8,26 +8,34 @@ import sympy
 # , belief agent console version,
 # check agains agm postulates
 
+# belief age index
+belief_age = 0
+knowledge_base = []
+
 def pl_resolution(KB, alpha):  # same as in book figure 7.13
     KB = sympify(KB)
     alpha = sympify(alpha)
-    combined_cnf = And(sympy.to_cnf(KB), sympy.to_cnf(Not(alpha)))
+    beliefs = alpha.args if isinstance(alpha, And) else [alpha]
 
-    clauses = set(combined_cnf.args)
-    new_clauses = set()
+    for belief in beliefs:
+        combined_cnf = And(sympy.to_cnf(KB), sympy.to_cnf(Not(belief)))
 
-    while True:
-        pairs = list(combinations(clauses, 2))
-        for (Ci, Cj) in pairs:
-            resolvents = pl_resolve(Ci, Cj)
-            if resolvents == {False}:  # contradiction (KB entails alpha)
-                return True
-            new_clauses |= resolvents
+        clauses = set(combined_cnf.args)
+        new_clauses = set()
 
-        if new_clauses.issubset(clauses):
-            return False
+        while True:
+            pairs = list(combinations(clauses, 2))
+            for (Ci, Cj) in pairs:
+                resolvents = pl_resolve(Ci, Cj)
+                if False in resolvents:  # contradiction (KB entails alpha)
+                    return True
+                new_clauses |= resolvents
 
-        clauses |= new_clauses
+            if new_clauses.issubset(clauses):
+                return False
+
+            clauses |= new_clauses
+    return False
 
 
 def pl_resolve(ci, cj):
@@ -44,17 +52,38 @@ def pl_resolve(ci, cj):
 
 
 def expand(KB, alpha):
-    return And(KB, alpha)
-
-
-def contract(KB, alpha):
+    global belief_age
+    belief_age += 1
+    KB.append((alpha, belief_age))
     return KB
-    # todo
+
+
+def contract(KB, notAlpha):
+    # Find all sets that do not imply alpha
+    remainder_sets = [(belief, age) for belief, age in KB if not pl_resolution(belief, notAlpha)]
+    print("Remainder sets")
+    print(remainder_sets)
+
+    # Sort the remainder_sets sets by belief age in ascending order
+    remainder_sets.sort(key=lambda x: x[1], reverse=True)
+
+    # Calculate the number of sets to include based on the percentage
+    if len(remainder_sets) < 100:
+        num_sets_to_include = len(remainder_sets)
+    else :
+        num_sets_to_include = int(len(remainder_sets) * 0.9)
+
+    # Intersect the remainder_sets based on their priority (i.e., belief age)
+    # Here we're using the fact that the sets are sorted by belief age to perform the intersection
+    contracted_kb = remainder_sets[:num_sets_to_include]
+    # Return the contracted knowledge base
+    return contracted_kb
 
 
 def revise(KB, alpha):  # levi identity
-    if pl_resolution(KB, alpha):  # tautology check
-        return KB
+    combined_beliefs = And(*[belief for belief, _ in KB])
+    if pl_resolution(combined_beliefs, alpha):  # tautology check
+        return expand(KB, alpha)
 
     contracted_kb = contract(KB, Not(alpha))
     expanded_kb = expand(contracted_kb, alpha)
@@ -73,20 +102,22 @@ expression2 = c
 
 # ------------ ADRIAN ---------------#
 # Set up an initial list for storing beliefs
-knowledge_base = []
+
 
 # Add a new belief to the knowledge base
-def include_belief(knowledge_base, new_belief):
+
+def include_belief(kb, new_belief):
+    global knowledge_base
+    global belief_age
     if knowledge_base:
-        # Combine all beliefs in the knowledge base into a single belief
-        combined_belief = And(*knowledge_base)
         # Revise the combined belief with the new belief
-        revised_belief = revise(combined_belief, new_belief)
-        # Clear the knowledge base and add the revised belief to it
+        revised_kb = revise(kb, new_belief)
         knowledge_base.clear()
-        knowledge_base.append(revised_belief)
+        knowledge_base = revised_kb
     else:
-        knowledge_base.append(new_belief)
+        global belief_age
+        belief_age += 1
+        knowledge_base.append((new_belief, belief_age))
 
 # Show all beliefs in the knowledge base
 def show_beliefs(knowledge_base):
